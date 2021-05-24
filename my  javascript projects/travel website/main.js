c6f254bbca762c83c6feb1cb4c63d054d6272eed
yash <yashsojitra97@gmail.com>
2021-05-24 17:37:44 +0530

    added animation to my page

--- my  javascript projects/travel website/main.js
+++ my  javascript projects/travel website/main.js
@@ -1,2 +1,40 @@
 let Controller;
 let slideScene;
+
+function animateSLides() {
+  //initializing Controller
+  Controller = new ScrollMagic.Controller();
+
+  //adding sliders and navbar
+  const sliders = document.querySelectorAll(".slide");
+  const nav = document.querySelector(".nav-bar");
+
+  //looping over all the sliders
+  sliders.forEach((slide) => {
+    const revealImg = slide.querySelector(".reveal-img");
+    const Img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-txt");
+
+    //generating a timeline
+    const slideTl = gsap.timeline({defaults:{duration:1, ease: 'power2.inOut'}})
+
+    slideTl.fromTo(revealImg, {x:'0%'},{x:'100%'})
+    slideTl.fromTo(revealText, {x:'0%'},{x:'100%'}, '-=0.8')
+    slideTl.fromTo(Img, {scale: '2'}, {scale: '1'}, '-=1')
+  });
+}
+
+//following cursor
+const mouse = document.querySelector(".cursor");
+
+function cursor(e) {
+  mouse.style.left = e.pageX + "px";
+  mouse.style.top = e.pageY + "px";
+}
+
+//Event Listners
+
+window.addEventListener("mousemove", cursor);
+
+//runnig functions
+animateSLides();
