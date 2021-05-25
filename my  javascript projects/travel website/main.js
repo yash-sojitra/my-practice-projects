dd8c7ea6790ace36a9ed8b198218ae595d0d6aab
yash <yashsojitra97@gmail.com>
2021-05-25 10:07:59 +0530

    just saving file to start today's work

--- my  javascript projects/travel website/main.js
+++ my  javascript projects/travel website/main.js
@@ -7,7 +7,7 @@ function animateSLides() {
 
   //adding sliders and navbar
   const sliders = document.querySelectorAll(".slide");
-  const nav = document.querySelector(".nav-bar");
+  const nav = document.querySelector("header");
 
   //looping over all the sliders
   sliders.forEach((slide) => {
@@ -16,11 +16,14 @@ function animateSLides() {
     const revealText = slide.querySelector(".reveal-txt");
 
     //generating a timeline
-    const slideTl = gsap.timeline({defaults:{duration:1, ease: 'power2.inOut'}})
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
 
-    slideTl.fromTo(revealImg, {x:'0%'},{x:'100%'})
-    slideTl.fromTo(revealText, {x:'0%'},{x:'100%'}, '-=0.8')
-    slideTl.fromTo(Img, {scale: '2'}, {scale: '1'}, '-=1')
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.8");
+    slideTl.fromTo(Img, { scale: "2" }, { scale: "1" }, "-=1");
+    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
   });
 }
 
