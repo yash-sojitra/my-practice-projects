5586a33bb9394b1b72b218784c4891d44ca1016f
yash <yashsojitra97@gmail.com>
2021-07-13 11:11:57 +0530

    just some changes

--- my  javascript projects/nodejs/expressApp.js
+++ my  javascript projects/nodejs/expressApp.js
@@ -1,6 +1,23 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const port = 80;
 
-const app = express()
-const port = 80
+app.get("/", (req, res) => {
+  res.status(200).send("this is my first express app");
+});
 
-app.get('/', )
+app.get("/about", (req, res) => {
+  res.status(200).send("this is about page of my first express app");
+});
+
+app.post("/about", (req, res) => {
+  res.status(200).send("this is about page by post of my first express app");
+});
+
+app.get("/this", (req, res) => {
+  res.status(404).send("page not found");
+});
+
+app.listen(port, () => {
+  console.log(`server successfully started at ${port}`);
+});
