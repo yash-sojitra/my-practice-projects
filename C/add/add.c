7f6ae134f933de239a53bd805393feb4eb5434db
yashsojitra97 <yashsojitra97@gmail.com>
2022-10-09 10:55:05 +0530

    new c commits

---
+++ C/add/add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("enter a: ");
+    scanf("%d", &a);
+
+    printf("enter b: ");
+    scanf("%d", &b);
+
+    printf("addition of a and b is: %d", a + b);
+
+    return 0;
+}
