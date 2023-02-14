c7da2254cd6a27db0d9d5eb38b4af744698732ec
yashsojitra97 <yashsojitra97@gmail.com>
2023-02-14 15:01:22 +0530

    lab commit

--- C/IIIT/mergearr.c
+++ C/IIIT/mergearr.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+void selectionSort(int arr[],int n){
+    for (int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            if(arr[j] < arr[i]){
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }   
+}
 void printArr(int arr[],int n){
     for (int i = 0; i < n; i++)
         printf("%d ",arr[i]);
@@ -16,6 +27,8 @@ int main(){
 
     for(int j=0; j<n2; j++)
         ans[n1+j] = arr2[j];
+
+    selectionSort(ans,n);
 
     printArr(ans,n);
     return 0;
--- C/IIIT/sort.c
+++ C/IIIT/sort.c
@@ -1,59 +1,54 @@
 #include <stdio.h>
 
-void selectionSort(int arr[],int n)
+void selectionSort(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        int min_index = i;
-
-        for(int j = i+1; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if(arr[j] < arr[min_index])
+            if (arr[j] < arr[i])
             {
-                min_index = j;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
-        int temp = arr[min_index];
-        arr[min_index] = arr[i];
-        arr[i] = temp;
     }
-    
 }
 
-void insertionSort(int arr[],int n)
+void insertionSort(int arr[], int n)
 {
     for (int i = 1; i < n; i++)
     {
         int key = arr[i];
-        int j = i-1;
+        int j = i - 1;
 
-        while (j>=0 && key < arr[j])
+        while (j >= 0 && key < arr[j])
         {
-            arr[j+1] = arr[j];
+            arr[j + 1] = arr[j];
             j--;
         }
 
-        arr[j+1] = key;
-    } 
+        arr[j + 1] = key;
+    }
 }
 
-void printArr(int arr[],int n)
+void printArr(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        printf("%d ",arr[i]);
+        printf("%d ", arr[i]);
     }
-    
 }
 
 int main()
 {
-    int arr[] = {2,4,5,6,3,6,7,8,8,9};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {2, 4, 5, 6, 3, 6, 7, 8, 8, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
     selectionSort(arr, n);
 
-    //insertionSort(arr, n);
+    // insertionSort(arr, n);
 
     printArr(arr, n);
     return 0;
