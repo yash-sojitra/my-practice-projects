5203310d8e0392f73aa2e8499be1113ad8724e2f
yashsojitra97 <yashsojitra97@gmail.com>
2023-04-04 12:04:46 +0530

    creating function for list creation and insertion at last

--- DSA/IIIT/linklist.c
+++ DSA/IIIT/linklist.c
@@ -1,54 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct Node{
+struct Node
+{
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
 typedef struct Node node;
 
-void printList(node* n){
-
+void printList(node *n)
+{
     while (n != NULL)
     {
-        printf("%d ",n->data);
+        printf("%d ", n->data);
         n = n->next;
     }
 }
 
-void insertFirst(node* head, node* n){
-    node* t1 = n->next;
-    head->next = n;
+void createList(node *head, int n)
+{
+    node *ptr;
+    ptr = head;
     
+    for (int i = 1; i < n; i++)
+    {
+        node *temp;
+        temp = (node *)malloc(sizeof(node));
+
+        scanf("%d", &temp->data);
+        temp->next = NULL;
+
+        ptr->next = temp;
+        ptr = temp;
+    }
 }
 
+void insertLast(node *head)
+{
+    node *temp, *ptr;
+
+    ptr = head;
+    temp = (node *)malloc(sizeof(node));
+
+    printf("enter the data of new node: \n");
+    scanf("%d", temp->data);
+    temp->next = NULL;
+
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+
+    ptr->next = temp;
+}
 
 int main()
-{  
+{
+    int n;
+    printf("enter length of linked list: \n");
+    scanf("%d",&n);
 
-    node* head = NULL;
-    node* second = NULL;
-    node* third = NULL;
-    node* new = NULL;
-    //node example;
+    node *head = NULL;
+    head = (node *)malloc(sizeof(node));
 
-    // example.data = 6;
-    // example.next = head;
+    printf("enter data of linked list: \n");
 
-    head = (node*)malloc(sizeof(node));
-    second = (node*)malloc(sizeof(node));
-    third = (node*)malloc(sizeof(node));
-
-    printf("enter data for 3 nodes: \n");
     scanf("%d", &head->data);
-    scanf("%d", &second->data);
-    scanf("%d", &third->data);
+    head->next = NULL;
 
-    head->next = second;
-    second->next = third;
-    third->next = NULL;
-    
+    createList(head, n);
+
+    printf("\nthe linked list is:\n");
     printList(head);
+    // insertLast(head);
+    // printf("\n");
+    // printList(head);
 
     return 0;
 }
