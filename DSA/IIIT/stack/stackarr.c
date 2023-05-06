f3a720c6201171f69f40f0542aefba77e599c587
yashsojitra97 <yashsojitra97@gmail.com>
2023-05-06 21:46:57 +0530

    lab files

---
+++ DSA/IIIT/stack/stackarr.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Stack
+{
+    int top;
+    int capacity;
+    int *array;
+} stack;
+
+int isFull(stack *stack)
+{
+    if (stack->top == stack->capacity - 1)
+        return 1;
+    return 0;
+}
+
+int isEmpty(stack *stack)
+{
+    if (stack->top == -1)
+        return 1;
+    return 0;
+}
+
+void push(stack *stack, int data)
+{
+    if (isFull(stack))
+    {
+        printf("\n!!! overflow !!!\n");
+        return;
+    }
+    else
+    {
+        stack->top++;
+        stack->array[stack->top] = data;
+    }
+}
+
+void pop(stack *stack)
+{
+    if (isEmpty(stack))
+    {
+        printf("\n!!! empty !!!\n");
+    }
+    else
+    {
+        stack->array[stack->top] = 0;
+        stack->top--;
+    }
+}
+
+void traverse(stack *stack)
+{
+
+    if (isEmpty(stack))
+    {
+        printf("\n!!! empty !!!\n");
+        return;
+    }
+
+    for (int i = stack->top; i >= 0; i--)
+    {
+        printf("%d\n", stack->array[i]);
+    }
+}
+
+int main()
+{
+    stack *numbers;
+    numbers = (stack *)malloc(sizeof(stack));
+    numbers->capacity = 5;
+    numbers->top = -1;
+    numbers->array = (int *)malloc(sizeof(int) * numbers->capacity);
+
+    while (1)
+    {
+        int choice, data;
+        printf("\n-->stack operations\n\n1. traversal\n2. push\n3. pop\n4. exit\n\nenter choice--> ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            traverse(numbers);
+            break;
+        case 2:
+            printf("enter the data you eant to push: \n");
+            scanf("%d", &data);
+            push(numbers, data);
+            break;
+        case 3:
+            pop(numbers);
+            break;
+        }
+        if (choice == 4)
+            break;
+    }
+
+    return 0;
+}
