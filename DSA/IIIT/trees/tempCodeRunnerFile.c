5e090aceb00ed298638dd32b5fc9406fecd69965
yashsojitra97 <yashsojitra97@gmail.com>
2023-05-14 23:03:56 +0530

    assignment - 5 commit

---
+++ DSA/IIIT/trees/tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+void leafValue(node* root){
+//     if(root == NULL)
+//         return;
+//     printf("%d ", root->data);
+//     preOrder(root->left);
+//     preOrder(root->right);
+// }
--- DSA/IIIT/trees/tree.c
+++ DSA/IIIT/trees/tree.c
@@ -1,45 +1,205 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
+typedef struct node
 {
     int data;
-    struct node* lChild;
-    struct node* rChild;
-};
+    struct node *left;
+    struct node *right;
+} node;
 
-int preOrder(struct node* root)
+void printTabs(int level)
 {
-    if (root != NULL)
+    for (int i = 0; i < level; i++)
+        printf("\t");
+}
+
+void printTree_rec(node *root, int level)
+{
+    if (root == NULL)
     {
-        printf("%d ", root->data);
-        preOrder(root->lChild);
-        preOrder(root->rChild);
-        return 0;
+        printTabs(level);
+        printf("---<empty>---\n");
+        return;
     }
+    printTabs(level);
+    printf("%d\n", root->data);
+    printTabs(level);
+    printf("left\n");
+    printTree_rec(root->left, level + 1);
+    printTabs(level);
+    printf("right\n");
+    printTree_rec(root->right, level + 1);
+}
+
+void printTree(node* root)
+{
+    int level = 0;
+    printTree_rec(root,level);
+}
+
+void height_rec(node *root, int level, int *lvlptr)
+{
+    if (level > *(lvlptr))
+        *(lvlptr) = level;
+
+    if (root == NULL)
+        return;
+
+    height_rec(root->left, level + 1, lvlptr);
+    height_rec(root->right, level + 1, lvlptr);
+}
+
+int height(node* root)
+{
+    int level = 0, *lvlptr = &level;
+    height_rec(root,level,lvlptr);
+    return level - 1;
+}
+
+void addNode(node *root, node *temp)
+{
+    if (temp->data > root->data)
+    {
+        if (root->right == NULL)
+        {
+            root->right = temp;
+        }
+        else
+        {
+            addNode(root->right, temp);
+        }
+    }
+    else
+    {
+        if (root->left == NULL)
+        {
+            root->left = temp;
+        }
+        else
+        {
+            addNode(root->left, temp);
+        }
+    }
+}
+
+node *createNode(int value)
+{
+    node *temp = malloc(sizeof(node));
+    temp->data = value;
+    temp->left = NULL;
+    temp->right = NULL;
+}
+
+void createBST(node *root, int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        node *temp = createNode(n);
+        addNode(root, temp);
+    }
+}
+
+void preOrder(node *root)
+{
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void inOrder(node *root)
+{
+    if (root == NULL)
+        return;
+    inOrder(root->left);
+    printf("%d ", root->data);
+    inOrder(root->right);
+}
+
+void postOrder(node *root)
+{
+    if (root == NULL)
+        return;
+    postOrder(root->left);
+    postOrder(root->right);
+    printf("%d ", root->data);
+}
+
+int maxPathLength(node *root)
+{
+    
+    int leftLevel = height(root->left);
+    int rightLevel = height(root->right);
+
+    if (root->left != NULL)
+        leftLevel++;
+    if (root->right != NULL)
+        rightLevel++;
+
+    return leftLevel + rightLevel;
+}
+
+void leafValue(node *root)
+{
+    if (root == NULL)
+        return;
+
+    if (root->left == NULL && root->right == NULL)
+        printf("%d ", root->data);
+    
+    leafValue(root->left);
+    leafValue(root->right);
 }
 
 int main()
 {
+    int num, first;
+    printf("enter no of elements\n");
+    scanf("%d", &num);
+    printf("enter elements\n");
+    scanf("%d", &first);
+    node *root = createNode(first);
+    createBST(root, num - 1);
 
-    struct node* root = (struct node*)malloc(sizeof(struct node));
-    struct node* l1 = (struct node*)malloc(sizeof(struct node));
-    struct node* r1 = (struct node*)malloc(sizeof(struct node));
-
-    root->data = 12;
-    l1->data = 20;
-    r1->data = 6;
-
-    l1->lChild = NULL;
-    l1->rChild = NULL;
-    r1->lChild = NULL;
-    r1->rChild = NULL;
-
-    root->lChild = l1;
-    root->rChild = r1;
-
-preOrder(root);
-
+    while (1)
+    {
+        int choice;
+        printf("\n\n-->list operations\n\n1. print tree\n2. pre-order\n3. in-order\n4. post-order\n5. height of tree\n6. maximun path length\n7. values of leaf nodes\n8. exit\n\nenter choice--> ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printTree(root);
+            break;
+        case 2:
+            printf("\npre-order traversal\n");
+            preOrder(root);
+            break;
+        case 3:
+            printf("\nin-order traversal\n");
+            inOrder(root);
+            break;
+        case 4:
+            printf("\npost-order traversal\n");
+            postOrder(root);
+            break;
+        case 5:
+            printf("\nheight of tree is:\n%d", height(root));
+            break;
+        case 6:
+            printf("\nmax path length:\n%d", maxPathLength(root));
+            break;
+        case 7:
+            printf("\nlist of all the leaf node values are:\n");
+            leafValue(root);
+            break;
+        }
+        if (choice == 8)
+            break;
+    }
     return 0;
 }
-//tree
