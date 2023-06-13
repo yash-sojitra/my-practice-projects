171dd8a855d3ec69d6292e0137d4f189343d9b1d
yashsojitra97 <yashsojitra97@gmail.com>
2023-06-14 00:05:26 +0530

    new project dependencies

--- web d resources/my-express-server/server.js
+++ web d resources/my-express-server/server.js
@@ -1,6 +1,15 @@
 const express = require("express")
-
 const app = express();
+
+app.get("/", function(request, response) {
+    response.send("hello");
+})
+app.get("/contact", function(request, response) {
+    response.send("contact me at: yashsojitra97@gmail.com");
+})
+app.get("/about", function(request, response) {
+    response.send("I am yash");
+})
 
 app.listen(3000, function() {
     console.log("hello from the server side");
